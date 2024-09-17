e2cb21f574125dbcd6e1728e4c1e85664cba2cbc
tebyarce <earce1999@gmail.com>
2024-09-17 16:50:01 -0600

    first commit, packages and structure

---
+++ src/main/java/com/estebanArce/RollJiuJitsu/RollJiuJitsuApplication.java
@@ -0,0 +1,13 @@
+package com.estebanArce.RollJiuJitsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollJiuJitsuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollJiuJitsuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estebanArce/RollJiuJitsu/RollJiuJitsuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estebanArce.RollJiuJitsu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RollJiuJitsuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
